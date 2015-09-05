6e21b12e1df7706789384c611751690347b98257
sunnystring <mkielholz@vtxmail.ch>
2015-09-05 17:31:30 +0200

    TableModel implemented on StudentDataEntry and ScheduleDataEntry, ready to start implementing dynamic data handling

--- src/dialogs/StudentDataEntry.java
+++ src/dialogs/StudentDataEntry.java
@@ -6,9 +6,13 @@
 package dialogs;
 
 import core.Main;
+import core.Student;
+
 import java.awt.BorderLayout;
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.BorderFactory;
@@ -16,11 +20,14 @@ import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
+import javax.swing.SwingConstants;
 import javax.swing.table.TableModel;
 import mainframe.MainFrame;
 
@@ -30,21 +37,24 @@ import mainframe.MainFrame;
  */
 public class StudentDataEntry extends JDialog {
 
-    private TableModel tableModel;
- 
-    private JPanel top, center, bottom;
-    private JLabel footnote;
+    private Student student;
+
+    private JPanel top, bottom;
+    private JScrollPane center;
+    private JLabel firstnameLabel, nameLabel, lectiontypeLabel, footnote;
     private JTextField firstname, name, lectiontype;
     private JTable selectionTable;
     private JButton cancel, save, delete;
 
+    private static JFrame owner; // = MainFrame
+
     public StudentDataEntry() {
 
-        super(Main.getMainFrame());
-        this.tableModel = Main.getMainFrame().getTableModel();
+        super(owner);
+        student = new Student();
 
+        setLocationRelativeTo(null);
         setModal(true);
-        setLocationRelativeTo(Main.getMainFrame());
         setTitle("Schülerprofil");
         setPreferredSize(new Dimension(500, 300));
         createWidgets();
@@ -56,25 +66,31 @@ public class StudentDataEntry extends JDialog {
 
     private void createWidgets() {
 
+        selectionTable = new JTable(student.getStudentTimeModel()); // TableModel (StudentTimes) übergeben
+        selectionTable.setShowGrid(true);
+        selectionTable.getColumnModel().setColumnSelectionAllowed(true); //  in alle Zellen kann geschrieben werden
+
         top = new JPanel();
         top.setLayout(new BoxLayout(top, BoxLayout.LINE_AXIS));
         top.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-        center = new JPanel();
+
+        center = new JScrollPane(selectionTable, JScrollPane.VERTICAL_SCROLLBAR_NEVER,
+                JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+
         bottom = new JPanel();
         bottom.setLayout(new BoxLayout(bottom, BoxLayout.LINE_AXIS));
         bottom.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 
+        firstnameLabel = new JLabel("Vorname:");
+        nameLabel = new JLabel("  Name:");
+        lectiontypeLabel = new JLabel("  Lektionsdauer:");
         footnote = new JLabel("* Beginn der Lektion");
         footnote.setFont(this.getFont().deriveFont(Font.PLAIN, 9));
 
-        firstname = new JTextField(" Vorname");
-        name = new JTextField(" Name");
-        lectiontype = new JTextField(" Lektionsdauer");
-
-        selectionTable = new JTable(tableModel);
-        selectionTable.setShowGrid(true);
-        selectionTable.getColumnModel().setColumnSelectionAllowed(true); //  in alle Zellen kann geschrieben werden
-        selectionTable.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+        firstname = new JTextField(" ");
+        name = new JTextField(" ");
+        lectiontype = new JTextField("30");
+        lectiontype.setMaximumSize(lectiontype.getPreferredSize());
 
         cancel = new JButton("Abbrechen");
         save = new JButton("Speichern");
@@ -84,11 +100,15 @@ public class StudentDataEntry extends JDialog {
 
     private void addWidgets() {
 
+        top.add(firstnameLabel);
         top.add(firstname);
+        top.add(Box.createHorizontalGlue());
+        top.add(nameLabel);
         top.add(name);
+        top.add(Box.createHorizontalGlue());
+        top.add(lectiontypeLabel);
         top.add(lectiontype);
-        center.add(BorderLayout.PAGE_START, selectionTable.getTableHeader());
-        center.add(BorderLayout.CENTER, selectionTable);
+
         bottom.add(footnote);
         bottom.add(Box.createHorizontalGlue());
         bottom.add(cancel);
@@ -103,18 +123,31 @@ public class StudentDataEntry extends JDialog {
 
     private void addListener() {
 
-        cancel.addActionListener(new CancelListener());
+        cancel.addActionListener(new CancelButtonListener());
         delete.addActionListener(null);
-        save.addActionListener(null);
+        save.addActionListener(new SaveButtonListener());
 
     }
 
-    private class CancelListener implements ActionListener {
+    public static void setOwner(JFrame mainframe) {
+        owner = mainframe;
+    }
+
+    private class CancelButtonListener implements ActionListener {
 
         @Override
         public void actionPerformed(ActionEvent e) {
 
             StudentDataEntry.this.dispose();
+        }
+    }
+
+    private class SaveButtonListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println(student.getStudentTime(0, 0));
+
         }
 
     }
